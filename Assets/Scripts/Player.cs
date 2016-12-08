656bbf38bc89d2190712a2b67646d9bd4d72171f
Horia Jurcut <horia.dragos@gmail.com>
2016-12-08 20:38:33 +0100

    Implement basic player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+
+    Camera viewCamera;
+    PlayerController playerController;
+
+	void Start () {
+        playerController = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+	}
+	
+	void Update () {
+        float rayDistance;
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+
+        playerController.Move(moveVelocity);
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 pointOfIntersection = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
+
+            playerController.LookAt(pointOfIntersection);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 myVelocity;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        myVelocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookAtPoint)
+    {
+        Vector3 heightCorrectionPoint = new Vector3(lookAtPoint.x, transform.position.y, lookAtPoint.z);
+        transform.LookAt(heightCorrectionPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + myVelocity * Time.fixedDeltaTime);
+    }
+}
