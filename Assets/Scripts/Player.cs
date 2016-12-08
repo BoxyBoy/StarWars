6666dc58ce8c5a67b077478a9c639799042351be
Horia Jurcut <horia.dragos@gmail.com>
2016-12-08 22:20:09 +0100

    Implement basic gun system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,35 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
 public class Player : MonoBehaviour {
 
     public float moveSpeed = 5f;
 
     Camera viewCamera;
     PlayerController playerController;
+    GunController gunController;
 
 	void Start () {
         playerController = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
         viewCamera = Camera.main;
 	}
 	
 	void Update () {
-        float rayDistance;
-
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-
+        // Movement Input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
 
         playerController.Move(moveVelocity);
+
+        // Look At Input
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
 
         if (groundPlane.Raycast(ray, out rayDistance))
         {
             Vector3 pointOfIntersection = ray.GetPoint(rayDistance);
-            Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
+            // Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
 
             playerController.LookAt(pointOfIntersection);
+        }
+
+        // Weapon Input
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
         }
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    float mySpeed = 10f;
+
+    public void SetSpeed(float speed)
+    {
+        mySpeed = speed;
+    }
+
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * mySpeed);
+	}
+}
