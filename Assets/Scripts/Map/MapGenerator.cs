8729511f7c1641d8b8312726378e76a4ce73d5f7
Horia Jurcut <horia.dragos@gmail.com>
2016-12-17 21:18:35 +0100

    Make enemies spawn at random positions

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -20,6 +20,8 @@ public class MapGenerator : MonoBehaviour {
 
     List<Coordinate> tileCoordinates;
     Queue<Coordinate> shuffledTileCoordinates;
+    Queue<Coordinate> shuffledOpenTileCoordinates;
+    Transform[,] tileMap;
 
     Map currentMap;
 
@@ -44,6 +46,17 @@ public class MapGenerator : MonoBehaviour {
     private Vector3 CoordinateToPosition(int x, int y)
     {
         return new Vector3(-currentMap.mapSize.x / 2f + 0.5f + x, 0, -currentMap.mapSize.y / 2f + 0.5f + y) * tileSize;
+    }
+
+    public Transform GetTileFromPosition(Vector3 position)
+    {
+        int x = Mathf.RoundToInt(position.x / tileSize + (currentMap.mapSize.x - 1) / 2f);
+        int y = Mathf.RoundToInt(position.z / tileSize + (currentMap.mapSize.y - 1) / 2f);
+
+        x = Mathf.Clamp(x, 0, tileMap.GetLength(0) - 1);
+        y = Mathf.Clamp(y, 0, tileMap.GetLength(1) - 1);
+
+        return tileMap[x, y];
     }
 
     private bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
@@ -88,6 +101,7 @@ public class MapGenerator : MonoBehaviour {
     public void GenerateMap()
     {
         currentMap = maps[mapIndex];
+        tileMap = new Transform[currentMap.mapSize.x, currentMap.mapSize.y];
         System.Random randomGenerator = new System.Random(currentMap.seed);
         GetComponent<BoxCollider>().size = new Vector3(currentMap.mapSize.x * tileSize, 0.1f, currentMap.mapSize.y * tileSize);
 
@@ -114,6 +128,7 @@ public class MapGenerator : MonoBehaviour {
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1f - outlinePercent) * tileSize;
                 newTile.parent = mapHolder;
+                tileMap[x, y] = newTile;
             }
         }
 
@@ -122,6 +137,8 @@ public class MapGenerator : MonoBehaviour {
 
         int obstacleCount = (int)(currentMap.mapSize.x * currentMap.mapSize.y * currentMap.obstaclePercent);
         int currentObstacleCount = 0;
+
+        List<Coordinate> allOpenCoordinates = new List<Coordinate>(tileCoordinates);
 
         for (int i = 0; i < obstacleCount; i++)
         {
@@ -145,6 +162,8 @@ public class MapGenerator : MonoBehaviour {
 
                 obstacleMaterial.color = Color.Lerp(currentMap.foregroundColor, currentMap.backgroundColor, colorPercent);
                 obstacleRenderer.sharedMaterial = obstacleMaterial;
+
+                allOpenCoordinates.Remove(randomCoordinate);
             }
             else
             {
@@ -152,6 +171,8 @@ public class MapGenerator : MonoBehaviour {
                 currentObstacleCount--;
             }
         }
+
+        shuffledOpenTileCoordinates = new Queue<Coordinate>(Shuffle.ShuffleArray(allOpenCoordinates.ToArray(), currentMap.seed));
 
         // Creating NavMesh Mask
         Transform navMeshMaskLeft = Instantiate(navMeshMaskPrefab, Vector3.left * (currentMap.mapSize.x + maxMapSize.x) / 4f * tileSize, Quaternion.identity) as Transform;
@@ -179,6 +200,14 @@ public class MapGenerator : MonoBehaviour {
         shuffledTileCoordinates.Enqueue(randomCoordinate);
 
         return randomCoordinate;
+    }
+
+    public Transform GetRandomOpenTile()
+    {
+        Coordinate randomOpenCoordinate = shuffledOpenTileCoordinates.Dequeue();
+        shuffledOpenTileCoordinates.Enqueue(randomOpenCoordinate);
+
+        return tileMap[randomOpenCoordinate.x, randomOpenCoordinate.y];
     }
 
     [System.Serializable]
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
@@ -7,28 +6,89 @@ public class Spawner : MonoBehaviour {
     public Wave[] waves;
     public Enemy enemy;
 
+    GameEntity playerEntity;
+    Transform playerTransform;
+    MapGenerator map;
     Wave currentWave;
+    Vector3 previousCampingPosition;
 
     int currentWaveNumber;
     int enemiesRemainingToSpawn;
     int enemiesRemainingAlive;
     float nextSpawnTime;
+    float timeBetweenCamping = 2f;
+    float campingThresholdDistance = 1.5f;
+    float nextCampingCheckTime;
+
+    bool isCamping;
+    bool isDisabled = false;
 
     private void Start()
     {
+        playerEntity = FindObjectOfType<Player>();
+        playerEntity.OnDeath += OnPlayerDeath;
+        playerTransform = playerEntity.transform;
+
+        nextCampingCheckTime = Time.time + timeBetweenCamping;
+        previousCampingPosition = playerTransform.position;
+
+        map = FindObjectOfType<MapGenerator>();
         NextWave();
     }
 
     private void Update()
     {
+        if (isDisabled) return;
+
+        if (Time.time > nextCampingCheckTime)
+        {
+            nextCampingCheckTime = Time.time + timeBetweenCamping;
+            isCamping = (Vector3.Distance(playerTransform.position, previousCampingPosition) < campingThresholdDistance);
+
+            previousCampingPosition = playerTransform.position;
+        }
+
         if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
         {
             enemiesRemainingToSpawn--;
             nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
 
-            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
-            spawnedEnemy.OnDeath += OnEnemyDeath;
+            StartCoroutine(SpawnEnemy());
         }
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        float spawnTimer = 0f;
+        float spawnDelay = 1f;
+        float tileFlashSpeed = 4f;
+
+        Transform randomOpenTile = map.GetRandomOpenTile();
+        if (isCamping)
+        {
+            randomOpenTile = map.GetTileFromPosition(playerTransform.position);
+        }
+
+        Material tileMaterial = randomOpenTile.GetComponent<Renderer>().material;
+        Color originalColor = tileMaterial.color;
+        Color flashColor = Color.red;
+
+        while (spawnTimer < spawnDelay)
+        {
+            tileMaterial.color = Color.Lerp(originalColor, flashColor, Mathf.PingPong(spawnTimer * tileFlashSpeed, 1f));
+
+            spawnTimer += Time.deltaTime;
+            yield return null;
+        }
+        
+
+        Enemy spawnedEnemy = Instantiate(enemy, randomOpenTile.position, Quaternion.identity) as Enemy;
+        spawnedEnemy.OnDeath += OnEnemyDeath;
+    }
+
+    private void OnPlayerDeath()
+    {
+        isDisabled = true;
     }
 
     private void OnEnemyDeath()
