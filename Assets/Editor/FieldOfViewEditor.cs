b10654d047ee36c3dca9029977116de98801421e
Horia Jurcut <horia.dragos@gmail.com>
2016-12-28 22:10:27 +0100

    Import FieldOfView functionality from other project

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+    private void OnSceneGUI()
+    {
+        if (target == null) return;
+
+        FieldOfView fieldOfView = (FieldOfView)target;
+
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fieldOfView.transform.position, Vector3.up, Vector3.forward, 360f, fieldOfView.viewRadius);
+
+        Vector3 viewAngleA = fieldOfView.DirectionFromAngle(-fieldOfView.viewAngle / 2, false);
+        Vector3 viewAngleB = fieldOfView.DirectionFromAngle(+fieldOfView.viewAngle / 2, false);
+
+        Handles.DrawLine(fieldOfView.transform.position, fieldOfView.transform.position + viewAngleA * fieldOfView.viewRadius);
+        Handles.DrawLine(fieldOfView.transform.position, fieldOfView.transform.position + viewAngleB * fieldOfView.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fieldOfView.visibleTargets)
+        {
+            if (visibleTarget == null || fieldOfView.transform == null) continue;
+            Handles.DrawLine(fieldOfView.transform.position, visibleTarget.position);
+        }
+    }
+}
